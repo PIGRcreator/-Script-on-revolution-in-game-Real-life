e519b4b1fb91f8c9bc2634e014f7424cef276a95
PIGRcreator <87482171+PIGRcreator@users.noreply.github.com>
2021-07-15 19:27:11 +0300

    Revopy

---
+++ Revoscript.py
@@ -0,0 +1,21 @@
+import war
+import civilwar
+import revolution
+import start
+from capitalism import socialism
+
+if power = true:
+    Print("Kill revolutionaries")
+    import kill 
+    
+    
+if power = false:
+    Print("Kill Power")
+    import Powerkill
+    print("Change name of the Countries")
+    a=input("Your CountryName")
+    
+    
+    
+    
+#There may be errors in this code. It cannot be started. It is written as a joke.
